251f7808fe3388f65bf363e51e53fa0f3f4f7648
Martin Cajthaml <martin.cajthaml@symbio.agency>
2021-06-28 09:44:11 +0200

    Copy from template

---
+++ next.config.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { i18n, images } = require('./symbio.config');
+const withPWA = require('next-pwa');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+    i18n,
+    images,
+    target: 'serverless',
+    future: {
+        webpack5: true,
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+    pwa: {
+        dest: 'public',
+        disable: process.env.NODE_ENV === 'development',
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src https: blob: data: 'unsafe-inline' 'unsafe-eval' http://localhost:3000",
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer-when-downgrade',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value:
+                            'geolocation=(self); notifications=(self); push=(self); sync-xhr=(self); speaker=(self); vibrate=(self); fullscreen=(self)',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains; preload',
+                    },
+                ],
+            },
+            {
+                source: '/storybook/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                ],
+            },
+        ];
+    },
+};
+
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
